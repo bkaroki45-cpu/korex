d4dbc994831a98238ee3e64479537fe0b8a8ce0f
brian <bkaroki45@gmail.com>
2026-08-25 21:40:22 +0300

    Add search engine discovery metadata

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 
-from .views import home
+from .sitemaps import StaticViewSitemap
+from .views import home, robots_txt
 
 admin.site.site_header = "CLOUDD 1 Administration"
 admin.site.site_title = "CLOUDD 1 Admin"
@@ -10,6 +12,8 @@ admin.site.index_title = "Operations dashboard"
 
 urlpatterns = [
     path("", home, name="home"),
+    path("robots.txt", robots_txt, name="robots_txt"),
+    path("sitemap.xml", sitemap, {"sitemaps": {"static": StaticViewSitemap}}, name="sitemap"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("markets/", include("markets.urls")),
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 
@@ -5,3 +6,11 @@ def home(request):
     if request.user.is_authenticated:
         return redirect("dashboard")
     return render(request, "core/home.html")
+
+
+def robots_txt(request):
+    sitemap_url = request.build_absolute_uri("/sitemap.xml")
+    return HttpResponse(
+        f"User-agent: *\nAllow: /\nSitemap: {sitemap_url}\n",
+        content_type="text/plain",
+    )
