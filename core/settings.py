890221c81c2f15fabfab55470d6850d17c2edab0
brian <bkaroki45@gmail.com>
2026-09-09 10:28:00 +0300

    Configure Django SMTP backend for Brevo

--- core/settings.py
+++ core/settings.py
@@ -141,15 +141,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-# Email
-# https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
-
-MAILERS = {
-    'default': {
-        'BACKEND': 'django.core.mail.backends.console.EmailBackend',
-    },
-}
-
 AUTH_USER_MODEL = "accounts.User"
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "dashboard"
