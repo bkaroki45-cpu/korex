006ba78f78a0305fe966cd71e6b8b85a3c059502
bkaroki45 <bkaroki45@gmail.com>
2026-09-23 09:52:32 +0300

    Refresh certificate asset across devices

--- core/templates/service-worker.js
+++ core/templates/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cloudd1-v1";
+const CACHE_NAME = "cloudd1-v2";
 const APP_SHELL = ["/static/css/base.css", "/static/js/base.js", "/static/manifest.webmanifest"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
